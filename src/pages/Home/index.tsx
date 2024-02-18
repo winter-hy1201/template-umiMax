c22e09c42f625c3480960361c0adde3dc869a810
曹翰宇 <907098258@qq.com>
2024-02-18 09:30:03 +0800

    feat:init

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,19 @@
+import Guide from '@/components/Guide';
+import { trim } from '@/utils/format';
+import { PageContainer } from '@ant-design/pro-components';
+import { useModel } from '@umijs/max';
+import styles from './index.less';
+
+const HomePage: React.FC = () => {
+  const { name } = useModel('global');
+  return (
+    <PageContainer ghost>
+      <div className={styles.container}>
+        <Guide name={trim(name)} />
+        <div className="m-5">666</div>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default HomePage;
