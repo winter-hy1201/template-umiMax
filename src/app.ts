0df30d0b355a8c161faef36c57f9152c375983b9
曹翰宇 <907098258@qq.com>
2024-02-18 16:17:20 +0800

    开启退出登录功能

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { RunTimeLayoutConfig } from '@umijs/max';
 // 运行时配置
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
@@ -6,11 +7,14 @@ export async function getInitialState(): Promise<{ name: string }> {
   return { name: '@umijs/max' };
 }
 
-export const layout = () => {
+//ProLayout配置项
+export const layout: RunTimeLayoutConfig = () => {
   return {
     logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
     menu: {
       locale: false,
     },
+
+    logout: () => {},
   };
 };
