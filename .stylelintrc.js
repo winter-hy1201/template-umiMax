b75f60d33c1a899941e9173a829d5b6073842c7a
曹翰宇 <907098258@qq.com>
2024-02-18 09:37:39 +0800

    feat: 1.去除commit-msg校验 2.添加tailwind文件声明

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,18 @@
 module.exports = {
   extends: require.resolve('@umijs/max/stylelint'),
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind'],
+      },
+    ],
+  },
+  'at-rule-no-unknown': [
+    true,
+    {
+      ignoreAtRules: ['tailwind'],
+    },
+  ],
 };
