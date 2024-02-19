8b9f8fcaf61e7d570507cede57d917f0a962d525
曹翰宇 <907098258@qq.com>
2024-02-19 11:57:50 +0800

    添加接口生成工具

--- config/config.ts
+++ config/config.ts
@@ -14,4 +14,20 @@ export default defineConfig({
   npmClient: 'pnpm',
   tailwindcss: {},
   reactQuery: {},
+  plugins: ['@umijs/max-plugin-openapi'],
+  openAPI: [
+    // {
+    //   requestLibPath: "import { request } from '@umijs/max'",
+    //   // 或者使用在线的版本
+    //   // schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
+    //   schemaPath: join(__dirname, 'oneapi.json'),
+    //   mock: false,
+    // },
+    {
+      requestLibPath: "import { request } from '@umijs/max'",
+      schemaPath:
+        'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
+      projectName: 'swagger',
+    },
+  ],
 });
