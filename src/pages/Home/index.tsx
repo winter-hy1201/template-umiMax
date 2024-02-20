7a9db173cc42d167b061e41d1deb282d38b93f24
曹翰宇 <907098258@qq.com>
2024-02-20 15:22:48 +0800

    1

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,10 @@
-import { useStore } from '@/store/index';
 import { PageContainer } from '@ant-design/pro-components';
-import { Button } from 'antd';
 
 const HomePage: React.FC = () => {
-  const count = useStore((state) => state.count);
-  const increment = useStore((state) => state.increment);
-  const removeAllCount = useStore((state) => state.removeAllCount);
-
   return (
     <PageContainer ghost>
       <div>
-        <div>Hello zustand</div>
-        <div>{count}</div>
-
-        <Button type="primary" onClick={increment}>
-          +1
-        </Button>
-        <Button onClick={removeAllCount}>归零</Button>
+        <div>Hello</div>
       </div>
     </PageContainer>
   );
